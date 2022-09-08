28a44c0ddf00595bd1fc593a3f1df3f1a9a90ca2
starkoka <pochikokatalk@gmail.com>
2022-09-08 23:59:41 +0900

    Embed test

--- index.js
+++ index.js
@@ -17,8 +17,57 @@ client.on("interactionCreate", async (interaction) => {
     if (!interaction.isCommand()) {
         return;
     }
+    const exampleEmbed = {
+        color: 0x0099ff,
+        title: 'Some title',
+        url: 'https://discord.js.org',
+        author: {
+            name: 'Some name',
+            icon_url: 'https://i.imgur.com/AfFp7pu.png',
+            url: 'https://discord.js.org',
+        },
+        description: 'Some description here',
+        thumbnail: {
+            url: 'https://i.imgur.com/AfFp7pu.png',
+        },
+        fields: [
+            {
+                name: 'Regular field title',
+                value: 'Some value here',
+            },
+            {
+                name: '\u200b',
+                value: '\u200b',
+                inline: false,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+        ],
+        image: {
+            url: 'https://i.imgur.com/AfFp7pu.png',
+        },
+        timestamp: new Date().toISOString(),
+        footer: {
+            text: 'Some footer text here',
+            icon_url: 'https://i.imgur.com/AfFp7pu.png',
+        },
+    };
+
     if (interaction.commandName === 'about') {
-        await interaction.reply('テスト');
+        await interaction.reply({ embeds: [exampleEmbed] });
     }
 });
 
